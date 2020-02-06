c0274864d1dbb8dec6182ff8340c600642d6cd12
Florian Wessels <f.wessels@bitmotion.de>
2020-02-06 19:25:36 +0100

    [FEATURE] Introduce TYPO3 v10.2 compatibility

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types = 1);
+
+/***
+ *
+ * This file is part of the "Static Info Tables (UK)" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2020 Florian Wessels <f.wessels@bitmotion.de>, Bitmotion GmbH
+ *
+ ***/
+
+return [
+    \SJBR\StaticInfoTables\Domain\Model\Country::class => [
+        'tableName' => 'static_countries',
+        'properties' => [
+            'shortNameId' => [
+                'fieldName' => 'cn_short_id',
+            ],
+        ],
+    ],
+    \SJBR\StaticInfoTables\Domain\Model\Currency::class => [
+        'tableName' => 'static_currencies',
+        'properties' => [
+            'nameId' => [
+                'fieldName' => 'cu_name_id',
+            ],
+            'subdivisionNameId' => [
+                'fieldName' => 'cu_sub_name_id',
+            ],
+        ],
+    ],
+    \SJBR\StaticInfoTables\Domain\Model\Language::class => [
+        'tableName' => 'static_languages',
+        'properties' => [
+            'nameId' => [
+                'fieldName' => 'cu_name_id',
+            ],
+        ],
+    ],
+    \SJBR\StaticInfoTables\Domain\Model\Territory::class => [
+        'tableName' => 'static_territories',
+        'properties' => [
+            'nameId' => [
+                'fieldName' => 'cu_name_id',
+            ],
+        ],
+    ],
+];
--- Configuration/TCA/Overrides/static_territories.php
+++ Configuration/TCA/Overrides/static_territories.php
@@ -2,12 +2,10 @@
 declare(strict_types = 1);
 defined('TYPO3_MODE') || die;
 
-$initialize = function ($dataSetName) {
-    $additionalFields = [
-        'tr_name_en' => 'tr_name_id',
-    ];
-
-    \Bitmotion\StaticInfoTablesId\Provider\TcaProvider::generateAndRegisterTca($additionalFields, $dataSetName);
-};
-$initialize('static_territories');
-unset($initialize);
+call_user_func(
+    function ($additionalFields, $dataSetName) {
+        \Bitmotion\StaticInfoTablesAr\Provider\TcaProvider::generateAndRegisterTca($additionalFields, $dataSetName);
+    },
+    ['tr_name_en' => 'tr_name_id'],
+    'static_territories'
+);
