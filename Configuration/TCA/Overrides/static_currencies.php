d0253b5d9f9e2c7b98a9825fdbf188802b793716
Florian Wessels <f.wessels@bitmotion.de>
2020-02-07 11:58:36 +0100

    [BUGFIX] Use proper namespace

--- Configuration/TCA/Overrides/static_currencies.php
+++ Configuration/TCA/Overrides/static_currencies.php
@@ -4,7 +4,7 @@ defined('TYPO3_MODE') || die;
 
 call_user_func(
     function ($additionalFields, $dataSetName) {
-        \Bitmotion\StaticInfoTablesAr\Provider\TcaProvider::generateAndRegisterTca($additionalFields, $dataSetName);
+        \Bitmotion\StaticInfoTablesId\Provider\TcaProvider::generateAndRegisterTca($additionalFields, $dataSetName);
     },
     ['cu_name_en' => 'cu_name_id', 'cu_sub_name_en' => 'cu_sub_name_id'],
     'static_currencies'
