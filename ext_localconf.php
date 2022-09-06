bc87f3d024f740f56055a17f4d8393980c15e3bc
Max Rösch <github@elbebass.com>
2022-09-06 13:59:10 +0200

    [TASK] Adapt dependecies for latest TYPO3 major version

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,11 @@ defined('TYPO3_MODE') || die;
 
 call_user_func(
     function($extensionKey) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/TypoScript/Extbase/setup.typoscript">');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTSconfig/Suggest.tsconfig">');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+            sprintf('@import \'EXT:%s/Configuration/TypoScript/Extbase/setup.typoscript\'', $extensionKey)
+        );
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            sprintf('@import \'EXT:%s/Configuration/PageTSconfig/Suggest.tsconfig\'', $extensionKey)
+        );
     }, \Bitmotion\StaticInfoTablesId\Extension::EXTENSION_KEY
 );
